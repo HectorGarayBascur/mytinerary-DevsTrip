273d7b50d825d174ca766b29e976d8cbd8e07099
Hector Garay <hectorgaraybascur@gmail.com>
2022-08-22 15:41:51 -0400

    adding props interval to carousel

--- mytinerary/src/components/EventsCarousel.js
+++ mytinerary/src/components/EventsCarousel.js
@@ -4,19 +4,19 @@ export default function EventsCarousel() {
     const items = [
         { url: "/New_York1.jpg", title: "New York" },
         { url: "/San_fco1.jpg", title: "San Francisco" },
+        { url: "/Alemania1.jpg", title: "Germany" },
+        { url: "/Suiza1.jpg", title: "Swiss" },
+        { url: "/New_York2.jpg", title: "New York" },
+        { url: "/San_fco2.jpg", title: "San Francisco" },
         { url: "/Alemania2.jpg", title: "Germany" },
-        { url: "/Suiza.jpg", title: "Swiss" },
-        { url: "/New_York1.jpg", title: "New York" },
-        { url: "/San_fco1.jpg", title: "San Francisco" },
-        { url: "/Alemania2.jpg", title: "Germany" },
-        { url: "/Suiza.jpg", title: "Swiss" },
-        { url: "/New_York1.jpg", title: "New York" },
-        { url: "/San_fco1.jpg", title: "San Francisco" },
-        { url: "/Alemania2.jpg", title: "Germany" },
-        { url: "/Suiza.jpg", title: "Swiss" },
+        { url: "/Suiza2.jpg", title: "Swiss" },
+        { url: "/Cancun1.jpg", title: "Cancun" },
+        { url: "/Francia3.jpg", title: "Francia" },
+        { url: "/Cancun2.jpg", title: "Cancun" },
+        { url: "/Francia4.jpg", title: "Francia" },
 
     ]
     return (
-        <Carousel data={items} range={4}/>
+        <Carousel data={items} range={4} interval={5}/>
     )
 }
