5505dd139b4b0eb871e0113cb6ad3001ced2880c
juanmoyano123 <moyanojjeronimo@gmail.com>
2022-08-22 17:06:32 -0300

    mockup pages

---
+++ mytinerary/src/pages/NewCities.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import FooterLayout from "../layouts/FooterLayout"
+
+export default function Cities() {
+  return (
+    <div>
+      
+    <FooterLayout>
+     
+       <h3>New Cities</h3>
+    
+    </FooterLayout>
+
+    </div>
+    
+  )
+}
