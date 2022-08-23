99eceaea7c0c1c42439abc9e852fec629c5b29a2
francoEmunoz <munozfranco011@gmail.com>
2022-08-23 15:04:18 -0300

    elimino layout

--- mytinerary/src/pages/Cities.js
+++ mytinerary/src/pages/Cities.js
@@ -1,17 +1,7 @@
 import React from 'react'
-import FooterLayout from "../layouts/FooterLayout"
 
 export default function Cities() {
   return (
-    <div>
-      
-    <FooterLayout>
-     
-       <h3>Cities</h3>
-    
-    </FooterLayout>
-
-    </div>
-    
+        <h3>Cities</h3>
   )
 }
