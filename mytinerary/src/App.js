7ff2b2e22301674519a7bb16e57bac6555f9b1c9
Hector Garay <hectorgaraybascur@gmail.com>
2022-08-23 10:09:25 -0400

    Adding routes tu app.js

--- mytinerary/src/App.js
+++ mytinerary/src/App.js
@@ -2,16 +2,22 @@ import Cities from './pages/Cities';
 import NewCities from "./pages/NewCities"
 import Hero from "./pages/Hero"
 import UnderConstruction from './pages/UnderContrstruction';
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+
 
 
 
 function App() {
   return (
     <div className="App">
-        <Hero/>
-      {/* <Cities/> */}
-      {/*  <NewCities/> */}
-      {/* <UnderConstruction/> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path= '/' element={<Hero/>} />
+          <Route path= '/cities' element={<Cities/>} />
+          <Route path= '/newcities' element={<NewCities/>} />
+          <Route path= '/underconstruction' element={<UnderConstruction/>} />     
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
