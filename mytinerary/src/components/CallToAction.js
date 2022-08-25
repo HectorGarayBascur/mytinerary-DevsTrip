a48d532244b6de24da349b05eb023a50334326a6
Hector Garay <hectorgaraybascur@gmail.com>
2022-08-25 01:15:20 -0400

    Adding a div for responsive desing

--- mytinerary/src/components/CallToAction.js
+++ mytinerary/src/components/CallToAction.js
@@ -1,14 +1,16 @@
 import '../styles/CallToAction.css'
-import {Link as LinkRouter} from 'react-router-dom'
+import { Link as LinkRouter } from 'react-router-dom'
 
 export default function Videos() {
   const buttonText = "Click here !"
   return (
-    <div className="videos">
+    <div className="videos-Call">
+      <div className="call-column">
         <video className="video-main" autoPlay muted loop src="/MytineraryHero.mp4"></video>
-      <div className="overlay">
-        <p className="p-video">Find your next trip</p>
-        <LinkRouter to='/cities' className='A-Welcome'>{buttonText.toUpperCase()}</LinkRouter>
+        <div className="overlay">
+          <p className="p-video">Find your next trip</p>
+          <LinkRouter to='/cities' className='A-Welcome'>{buttonText.toUpperCase()}</LinkRouter>
+        </div>
       </div>
     </div>
   )
