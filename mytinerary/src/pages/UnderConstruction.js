f8fa3872b4bc3f8852364310760874305404ef1b
Hector Garay <hectorgaraybascur@gmail.com>
2022-08-25 01:20:22 -0400

    adding div for responsive desing

--- mytinerary/src/pages/UnderConstruction.js
+++ mytinerary/src/pages/UnderConstruction.js
@@ -5,10 +5,12 @@ export default function UnderConstruction() {
   const buttonText = "Go back to cities"
   return (
     <div className="videos-underconstruction">
+      <div className="video-column">  
         <video className="video-underconstruction" autoPlay muted loop src="/Underconstruction.mp4"></video>
       <div className="overlay-underconstruction">
         <p className="p-video-underconstruction">Don't be mad, other cities are waiting for you!</p>
         <LinkRouter to='/cities' className='A-Welcome-underconstruction'>{buttonText.toUpperCase()}</LinkRouter>
+      </div>
       </div>
     </div>
   )
