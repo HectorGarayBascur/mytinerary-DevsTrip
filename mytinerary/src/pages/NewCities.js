5f01e8d7017ada2406907c66a217c7d527acba80
juanmoyano123 <moyanojjeronimo@gmail.com>
2022-08-25 14:27:40 -0300

    formulario para agregar ciudades con funcion integrada

--- mytinerary/src/pages/NewCities.js
+++ mytinerary/src/pages/NewCities.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import InputText from '../components/InputText'
 
 export default function Cities() {
   return ( 
+
+    <div>
        <h3>New Cities</h3>
+       <InputText/>
+    </div>
   )
 }
