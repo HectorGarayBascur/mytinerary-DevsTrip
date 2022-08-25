bc9d1ba4f93c8692744c83b84970a3619cfd235c
Hector Garay <hectorgaraybascur@gmail.com>
2022-08-25 01:19:12 -0400

    changes to onClick event

--- mytinerary/src/components/ScrollToTopPage.js
+++ mytinerary/src/components/ScrollToTopPage.js
@@ -1,12 +1,10 @@
-import { Link as LinkRouter } from 'react-router-dom'
-import { useLocation } from 'react-router-dom'
-
 export default function ScrollToTopPage() {
-  const { pathname } = useLocation()
-  window.scrollTo({ top: 0, behavior: 'smooth' })
+  function scroll () {
+      window.scrollTo({ top: 0, behavior: 'smooth' })
+  }
   return (
     <div className='scroll-top'>
-      <LinkRouter to={pathname}><img className='icon-top' src="../arrowtop.png" alt=""></img></LinkRouter>
+      <img className='icon-top' src="../arrowtop.png" alt="scrolltopage" height="18px" onClick={scroll}></img>
     </div>
   )
 }
