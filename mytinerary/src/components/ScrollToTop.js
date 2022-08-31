d4d1eca581e367f1c124ab9bc3ea7abb2630de07
Hector Garay <hectorgaraybascur@gmail.com>
2022-08-31 08:50:10 -0400

    nothing change

--- mytinerary/src/components/ScrollToTop.js
+++ mytinerary/src/components/ScrollToTop.js
@@ -3,14 +3,13 @@ import { useLocation } from 'react-router-dom'
 
 export default function ScrollToTop() {
 
-    const { pathname } = useLocation()
-    console.log(pathname)
+  const { pathname } = useLocation()
 
-    useEffect(() => {
+  useEffect(() => {
 
-        window.scrollTo(0, 0);
+    window.scrollTo(0, 0);
 
-    }, [pathname])
+  }, [pathname])
 
   return (
     <></>
