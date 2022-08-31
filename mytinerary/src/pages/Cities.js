aef4e7d969d50e33ed4c9c08d7d874f0cd8770d4
juanmoyano123 <moyanojjeronimo@gmail.com>
2022-08-31 20:35:03 -0300

    agrego clase para dar estilos

--- mytinerary/src/pages/Cities.js
+++ mytinerary/src/pages/Cities.js
@@ -6,21 +6,20 @@ import { useEffect, useState, useRef } from 'react'
 
 
 export default function Cities() {
-const [items, setItems] = useState([])
-const[value, setValue] = useState("")
-const inputSearch = useRef()
+  const [items, setItems] = useState([])
+  const [value, setValue] = useState("")
+  const inputSearch = useRef()
 
 
   useEffect(() => {
-    axios.get('http://localhost:4000/cities?city='+value)
-      .then(response => 
-        {setItems(response.data)})
-        .catch(error => console.log(error))
+    axios.get('http://localhost:4000/cities?city=' + value)
+      .then(response => { setItems(response.data) })
+      .catch(error => console.log(error))
   }, [value]);
 
 
 
-  const valueCity =()=>{
+  const valueCity = () => {
     setValue(inputSearch.current.value)
   }
 
@@ -28,21 +27,15 @@ const inputSearch = useRef()
     <div>
       <div className='searchCities'>
         <form className='input-cities' method='get'>
-
-        <input  className="inputText" placeholder={'Search City'} ref={inputSearch}  onChange={valueCity} />
-       
+          <input className="inputText" placeholder={'Search City'} ref={inputSearch} onChange={valueCity} />
         </form>
-        
       </div>
-
-
-
       <div className='container-cities'>
         {items.map((item) => (
-          <CityCard key={item._id} 
-          city={item.city} 
-          photo={item.photo} 
-          country={item.country} />
+          <CityCard key={item._id}
+            city={item.city}
+            photo={item.photo}
+            country={item.country} />
         ))}
       </div>
     </div>
