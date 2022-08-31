975c62ba755e7ec5304b3d83aae499a234af0faa
Hector Garay <hectorgaraybascur@gmail.com>
2022-08-31 08:50:52 -0400

    import axios for interact with database

--- mytinerary/src/pages/Cities.js
+++ mytinerary/src/pages/Cities.js
@@ -1,33 +1,42 @@
 import React from 'react'
+import axios from 'axios'
 import CityCard from '../components/CityCard'
 import InputText from '../components/InputText'
+import { useEffect, useState } from 'react'
+
 
 export default function Cities() {
-  const items = [
-    { _id: 1, url: "/New_York1.jpg", title: "New York", description: "City description" },
-    { _id: 2, url: "/San_fco1.jpg", title: "San Francisco", description: "City description" },
-    { _id: 3, url: "/Alemania1.jpg", title: "Germany", description: "City description" },
-    { _id: 4, url: "/Suiza1.jpg", title: "Swiss", description: "City description" },
-    { _id: 5, url: "/New_York2.jpg", title: "New York", description: "City description" },
-    { _id: 6, url: "/San_fco2.jpg", title: "San Francisco", description: "City description" },
-    { _id: 7, url: "/Alemania2.jpg", title: "Germany", description: "City description" },
-    { _id: 8, url: "/Suiza2.jpg", title: "Swiss", description: "City description" },
-    { _id: 9, url: "/Cancun1.jpg", title: "Cancun", description: "City description" },
-    { _id: 10, url: "/Francia3.jpg", title: "Francia", description: "City description" },
-    { _id: 11, url: "/Cancun2.jpg", title: "Cancun", description: "City description" },
-    { _id: 12, url: "/Francia4.jpg", title: "Francia", description: "City description" },
-  ]
+  const [items, setItems] = useState([])
+  // { _id: 1, url: "/New_York1.jpg", title: "New York", description: "City description", country: "USA", population: "10.000", fundation: "2022" },
+  //   { _id: 2, url: "/San_fco1.jpg", title: "San Francisco", description: "City description", country: "USA", population: "10.000", fundation: "2022" },
+  //   { _id: 3, url: "/Alemania1.jpg", title: "Germany", description: "City description", country: "Germany", population: "10.000", fundation: "2022" },
+  //   { _id: 4, url: "/Suiza1.jpg", title: "Swiss", description: "City description", country: "Swiss", population: "10.000", fundation: "2022" },
+  //   { _id: 5, url: "/New_York2.jpg", title: "New York", description: "City description", country: "USA", population: "10.000", fundation: "2022" },
+  //   { _id: 6, url: "/San_fco2.jpg", title: "San Francisco", description: "City description", country: "USA", population: "10.000", fundation: "2022" },
+  //   { _id: 7, url: "/Alemania2.jpg", title: "Germany", description: "City description", country: "Germany", population: "10.000", fundation: "2022" },
+  //   { _id: 8, url: "/Suiza2.jpg", title: "Swiss", description: "City description", country: "Swiss", population: "10.000", fundation: "2022" },
+  //   { _id: 9, url: "/Cancun1.jpg", title: "Cancun", description: "City description", country: "Mexico", population: "10.000", fundation: "2022" },
+  //   { _id: 10, url: "/Francia3.jpg", title: "Francia", description: "City description", country: "France", population: "10.000", fundation: "2022" },
+  //   { _id: 11, url: "/Cancun2.jpg", title: "Cancun", description: "City description", country: "Mexico", population: "10.000", fundation: "2022" },
+  //   { _id: 12, url: "/Francia4.jpg", title: "Francia", description: "City description", country: "France", population: "10.000", fundation: "2022" },
+  // ]
 
+  useEffect(() => {
+    axios.get('http://localhost:4000/cities')
+      .then(response => setItems(response.data))
+  }, []);
 
-  
 
   return (
     <div>
-      <InputText/>
-      
-      {items.map((item) => {
-        return <CityCard key={item._id} id={item._id} title={item.title} url={item.url} description={item.description} />
-      })}
+      <div>
+        <InputText />
+      </div>
+      <div className='container-cities'>
+        {items.map((item) => (
+          <CityCard key={item._id} city={item.city} photo={item.photo} country={item.country} />
+        ))}
+      </div>
     </div>
   )
 }
