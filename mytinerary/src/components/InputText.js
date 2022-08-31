33e52521e48b80176e1c60f5df27fe5fd0fac575
Hector Garay <hectorgaraybascur@gmail.com>
2022-08-31 08:49:36 -0400

    deleted error of import cities css

--- mytinerary/src/components/InputText.js
+++ mytinerary/src/components/InputText.js
@@ -1,25 +1,24 @@
-import React, {useState} from 'react'
-import Cities from '../styles/Cities.css'
+import React, { useState } from 'react'
 
 export default function InputText() {
 
-const [city, setCity]= useState(" ")
-const handleCity =(e)=> {
-const city = e.target.value
-setCity(city)
-console.log(city)
-}
+  const [city, setCity] = useState(" ")
+  const handleCity = (e) => {
+    const city = e.target.value
+    setCity(city)
+    console.log(city)
+  }
 
 
   return (
-    
-        <form className='formText'>
-  <label id='city' className='labelForm'>
-    Citie Name:
-    <input type="text" for="" name="name" value={city} onChange={handleCity} className="inputText" />
-  </label>
 
-</form>
-    
+    <form className='formText'>
+      <label id='city' className='labelForm'>
+        Citie Name:
+        <input type="text" name="name" value={city} onChange={handleCity} className="inputText" />
+      </label>
+
+    </form>
+
   )
 }
