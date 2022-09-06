d6eecd16bc1c18631ace96361ef426fe5d62b6e0
Hector Garay <hectorgaraybascur@gmail.com>
2022-09-06 09:54:58 -0300

    using API from redux

--- mytinerary/src/pages/Cities.js
+++ mytinerary/src/pages/Cities.js
@@ -3,35 +3,69 @@ import axios from 'axios'
 import CityCard from '../components/CityCard'
 import '../styles/Cities.css'
 import { useEffect, useState, useRef } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchFromServer } from '../features/citySlice'
+import { useGetAllCitiesQuery } from '../features/citiesAPI'
 
 
 export default function Cities() {
-  const [items, setItems] = useState([])
+  const {
+    data: items,
+    error,
+    isLoading,
+    isSuccess,
+    isFailed,
+  } = useGetAllCitiesQuery();
+
+  // const [items, setItems] = useState([])
+  // let items = useSelector(state => state.cities.cities)
+  // let dispatch = useDispatch()
+
+  // useEffect(() => {
+  //   dispatch(fetchFromServer())
+  // }, [])
+  // console.log('Cities', items)
   const [value, setValue] = useState("")
-  const inputSearch = useRef()
+  // const inputSearch = useRef()
 
 
-  useEffect(() => {
-    axios.get('http://localhost:4000/cities?city=' + value)
-      .then(response => { setItems(response.data) })
-      .catch(error => console.log(error))
-  }, [value]);
+  // useEffect(() => {
+  //   axios.get('http://localhost:4000/cities?city=' + value)
+  //     .then(response => { setItems(response.data) })
+  //     .catch(error => console.log(error))
+  // }, [value]);
 
-
-
-  const valueCity = () => {
-    setValue(inputSearch.current.value)
+  const valueCity = (event) => {
+    setValue(event.target.value)
   }
+
+  // const filteredCities = (items || []).filter((item) => {
+  //   return item.city.toLowerCase().includes(value.toLowerCase())
+  // }) 
+
+  const filteredCities = []
+  items?.forEach(item => {
+    //la city tiene que contener el input
+
+    if (item.city.toLowerCase().includes(value.toLowerCase())) {
+      filteredCities.push(item)
+    }
+
+  });
+  console.log(filteredCities)
+
+  // ref={inputSearch}
+
 
   return (
     <div>
       <div className='searchCities'>
         <form className='input-cities' method='get'>
-          <input className="inputText" placeholder={'Search City'} ref={inputSearch} onChange={valueCity} />
+          <input className="inputText" placeholder={'Search City'} onChange={valueCity} />
         </form>
       </div>
       <div className='container-cities'>
-        {items.map((item) => (
+        {filteredCities.map((item) => (
           <CityCard key={item._id}
             city={item.city}
             photo={item.photo}
