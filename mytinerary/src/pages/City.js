607d9411f11c7b99acaf220f4332b9d8d24f52f7
francoEmunoz <munozfranco011@gmail.com>
2022-09-07 12:32:14 -0300

    agrego comp itinerary

--- mytinerary/src/pages/City.js
+++ mytinerary/src/pages/City.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import '../styles/City.css'
 import { Link as LinkRouter } from 'react-router-dom';
 import axios from 'axios';
+import Itinerary from '../components/Itinerary';
 
 export default function City() {
     const { id } = useParams()
@@ -17,40 +18,43 @@ export default function City() {
     }, [])
 
     return (
-        <div className='container-card-details'>
-            <div>
-                <h2>{cityData.country}</h2>
-            </div>
-            <div className="container-city">
-                <div className="card">
-                    <div className="card-header">
-                        <img src={cityData.photo} alt={cityData.city} />
-                    </div>
-                    <div className="card-body">
-                        {/* <span class="tag tag-teal">{cityData.country}</span> */}
-                        <h3>
-                            {cityData.city}
-                        </h3>
-                        <p>
-                            {cityData.description}
-                        </p>
-                        <div className="user">
-                            {/* <img src="https://yt3.ggpht.com/a/AGF-l7-0J1G0Ue0mcZMw-99kMeVuBmRxiPjyvIYONg=s900-c-k-c0xffffffff-no-rj-mo" alt="user" /> */}
-                            <div className="user-info">
-                                <h5>Population</h5>
-                                <small>{cityData.population}</small>
-                            </div>
-                            <div className="user-info">
-                                <h4 className="go-cities"><LinkRouter to='/cities'>Go to cities</LinkRouter></h4>
-                            </div>
-                            <div className="user-info">
-                                <h5>Fundation</h5>
-                                <small>{date.getFullYear()}</small>
+        <div>
+            <div className='container-card-details'>
+                <div>
+                    <h2>{cityData.country}</h2>
+                </div>
+                <div className="container-city">
+                    <div className="card">
+                        <div className="card-header">
+                            <img src={cityData.photo} alt={cityData.city} />
+                        </div>
+                        <div className="card-body">
+                            {/* <span class="tag tag-teal">{cityData.country}</span> */}
+                            <h3>
+                                {cityData.city}
+                            </h3>
+                            <p>
+                                {cityData.description}
+                            </p>
+                            <div className="user">
+                                {/* <img src="https://yt3.ggpht.com/a/AGF-l7-0J1G0Ue0mcZMw-99kMeVuBmRxiPjyvIYONg=s900-c-k-c0xffffffff-no-rj-mo" alt="user" /> */}
+                                <div className="user-info">
+                                    <h5>Population</h5>
+                                    <small>{cityData.population}</small>
+                                </div>
+                                <div className="user-info">
+                                    <h4 className="go-cities"><LinkRouter to='/cities'>Go to cities</LinkRouter></h4>
+                                </div>
+                                <div className="user-info">
+                                    <h5>Fundation</h5>
+                                    <small>{date.getFullYear()}</small>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            <Itinerary />
         </div>
     )
 }
