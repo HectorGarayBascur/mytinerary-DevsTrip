0e196b5b12475edb0a1cce16e1bbd62d5241ec32
francoEmunoz <munozfranco011@gmail.com>
2022-09-07 21:11:10 -0300

    activities

--- mytinerary/src/components/Activities.js
+++ mytinerary/src/components/Activities.js
@@ -3,13 +3,13 @@ import '../styles/Activities.css'
 export default function Activities({ activity }) {
 
     return (
-        <div className='container-activities-padre'>
-            <div className='container-activities'>
+            <div>
                 <div className='activity1'>
-                    <img src={activity.photo} alt=""></img>
+                    <div className='img-activity'>
+                        <img src={activity.photo} alt=""></img>
+                    </div>
                     <h3>{activity.name}</h3>
                 </div>
             </div>
-        </div>
     )
 }
