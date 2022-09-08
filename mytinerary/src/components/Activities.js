e7c57574148e62433a1cdb139ad282f92473cec7
Hector Garay <hectorgaraybascur@gmail.com>
2022-09-08 09:23:33 -0300

    Adding dynamic activity card

--- mytinerary/src/components/Activities.js
+++ mytinerary/src/components/Activities.js
@@ -3,13 +3,13 @@ import '../styles/Activities.css'
 export default function Activities({ activity }) {
 
     return (
-            <div>
-                <div className='activity1'>
-                    <div className='img-activity'>
-                        <img src={activity.photo} alt=""></img>
-                    </div>
-                    <h3>{activity.name}</h3>
+        <div>
+            <div className='activity1'>
+                <div className='img-activity'>
+                    <img src={activity.photo} alt=""></img>
                 </div>
+                <h3>{activity.name}</h3>
             </div>
+        </div>
     )
 }
