8763b6f6bab04ca4c6ea0052b40e5915bbf335fc
francoEmunoz <munozfranco011@gmail.com>
2022-09-12 15:24:43 -0300

    guardo API

--- mytinerary/src/features/store.js
+++ mytinerary/src/features/store.js
@@ -3,12 +3,14 @@ import citySlice from './citySlice'
 import citiesAPI from './citiesAPI'
 import itineraiesAPI from './itinerariesAPI'
 import activitiesAPI from './activitiesAPI'
+import commentsAPI from './commentsAp'
 
 export default configureStore({
     reducer: {
         cities: citySlice,
         [citiesAPI.reducerPath]: citiesAPI.reducer,
         [itineraiesAPI.reducerPath]: itineraiesAPI.reducer,
-        [activitiesAPI.reducerPath]: activitiesAPI.reducer
+        [activitiesAPI.reducerPath]: activitiesAPI.reducer,
+        [commentsAPI.reducerPath]: commentsAPI.reducer
     },
 })
