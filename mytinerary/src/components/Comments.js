ec23fa017102bb0f634b14de05abf79dcf26e690
juanmoyano123 <76905472+juanmoyano123@users.noreply.github.com>
2022-09-12 17:49:08 -0300

    script de google

--- mytinerary/src/components/Comments.js
+++ mytinerary/src/components/Comments.js
@@ -12,15 +12,13 @@ export default function Comments() {
         }
     }
 
-
     return (
         <div className="container-comments">
             <h2>Comments</h2>
             <img className="icon-despleg" onClick={openComments} src="https://cdn-icons-png.flaticon.com/512/3519/3519316.png" alt="" width='25px'></img>
             <div className='container2-comments'>
                 {
-                    open
-                        ? <div className='container3-comments'>
+                    open? <div className='container3-comments'>
                             <div className="img-user">
                                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSClJXC4X-mbfNq5t_GI45AWQMayCy_2qrKIigB1d83LL6JD3pS2YbKeFcr64Ix4GhY-wI&usqp=CAU" alt=""></img>
                             </div>
@@ -32,8 +30,7 @@ export default function Comments() {
                                     <p>epic</p>
                                 </div>
                             </div>
-                        </div>
-                        : null
+                        </div>: null
                 }
             </div>
         </div>
