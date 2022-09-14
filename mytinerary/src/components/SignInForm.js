2db53e9bf9aeae0e45b5919262e3f3a0ed98a9fb
juanmoyano123 <76905472+juanmoyano123@users.noreply.github.com>
2022-09-14 10:52:37 -0300

    boton de inicio de sesion con google

---
+++ mytinerary/src/components/SignInForm.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { useGetLoginMutation } from "../features/usersAPI";
+import SignInGoogle from "../components/SignInGoogle";
+import { useRef } from "react";
+
+function Input({ label, name }) {
+  return (
+    <label className="form-label">
+      {label}
+      <input name={name} />
+    </label>
+  );
+}
+
+export default function SignInForm() {
+  const buttonDiv = useRef(null);
+  const form = useRef();
+
+  const [userlog] = useGetLoginMutation();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const loginForm = new FormData(form.current);
+    const loginDataUser = {
+      mail: loginForm.get("mail"),
+      password: loginForm.get("password"),
+      from: "form",
+    };
+    await userlog(loginDataUser);
+    form.current.reset();
+    console.log(userlog);
+  };
+
+  return (
+    <div>
+      <form className="form-class" ref={form}>
+        <Input label="Mail:" name="mail" />
+        <Input label="Password:" name="password" />
+
+        <button type="submit" onClick={handleSubmit}>
+          Login!
+        </button>
+        <SignInGoogle />
+      </form>
+    </div>
+  );
+}
---
+++ mytinerary/src/components/SignInGoogle.js
@@ -0,0 +1,41 @@
+import { useEffect, useRef } from 'react'
+import * as jose from "jose"
+
+
+export default function SignUpGoogle() {
+  const buttonDiv = useRef(null)
+  console.log(buttonDiv.current)
+
+  async function handleCredentialResponse(response) {
+    let userObject = jose.decodeJwt(response.credential)
+    console.log(userObject)
+    //console.log( (buttonDiv.current))
+
+    let data = {
+      mail: userObject.mail,
+      password: userObject.password,
+      from: "google"
+    }
+
+    console.log(data)
+  }
+
+  useEffect(() => {
+    /*global google*/
+    google.accounts.id.initialize({
+      client_id: '954399506520-00mttq8tfcsq17roid4r4hmcri03g2gq.apps.googleusercontent.com',
+      callback: handleCredentialResponse,
+      context: "signin"
+    });
+    google.accounts.id.renderButton(
+      buttonDiv.current,
+      { theme: "outline", size: "medium", text: "signin_width" }  // customization attributes
+    );
+  }, [])
+  return (
+    <div>
+
+      <div ref={buttonDiv}></div>
+    </div>
+  )
+}
