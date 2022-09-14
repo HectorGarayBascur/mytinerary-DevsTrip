c9f7330ad205e92e7cc0be2a460c512e7a1c1810
francoEmunoz <munozfranco011@gmail.com>
2022-09-14 18:44:06 -0300

    page SignIn

---
+++ mytinerary/src/pages/SignIn.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../styles/SignUp.css"
+import SignInForm from '../components/SignInForm'
+
+export default function SignIn() {
+  return (
+
+    <div className='form-newcity'>
+    <SignInForm/>
+    </div>
+  )
+}
