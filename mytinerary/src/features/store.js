e8c9af1c45ac7774afe12aac55c157ec5f9df751
Hector Garay <hectorgaraybascur@gmail.com>
2022-09-15 11:01:03 -0300

    Adding authReducer to store

--- mytinerary/src/features/store.js
+++ mytinerary/src/features/store.js
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
+import authReducer from './authSlice'
 import citySlice from './citySlice'
 import citiesAPI from './citiesAPI'
 import itineraiesAPI from './itinerariesAPI'
@@ -9,6 +10,7 @@ import usersAPI from './usersAPI'
 export default configureStore({
     reducer: {
         cities: citySlice,
+        auth: authReducer,
         [citiesAPI.reducerPath]: citiesAPI.reducer,
         [itineraiesAPI.reducerPath]: itineraiesAPI.reducer,
         [activitiesAPI.reducerPath]: activitiesAPI.reducer,
@@ -16,8 +18,8 @@ export default configureStore({
         [commentsAPI.reducerPath]: commentsAPI.reducer
 
     },
-    middleware: (getAllCities)=> getAllCities({
-        immutableCheck:false,
-        serializableCheck:false
+    middleware: (getAllCities) => getAllCities({
+        immutableCheck: false,
+        serializableCheck: false
     })
 })
