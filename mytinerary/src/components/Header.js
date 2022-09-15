21ee0abe554d5b94fb75eb4111c928175eeff5f0
Hector Garay <hectorgaraybascur@gmail.com>
2022-09-15 10:50:50 -0300

    adding private key to pages for filter by role

--- mytinerary/src/components/Header.js
+++ mytinerary/src/components/Header.js
@@ -1,13 +1,17 @@
-import '../styles/Header.css'
-import { Link as LinkRouter } from 'react-router-dom'
-import Login from './Login'
 import { useState } from 'react'
+import { Link as LinkRouter } from 'react-router-dom'
+
+import Login from './Login'
+import { useAuth } from '../hooks/useAuth';
+
+import '../styles/Header.css'
 
 const pages = [
   { _id: 1, name: 'Home', to: '/' },
   { _id: 2, name: 'Cities', to: '/cities' },
-  { _id: 3, name: 'New Cities', to: '/newcities' },
-  { _id: 5, name: 'Edit Cities', to: '/editcities' },
+  { _id: 3, name: 'New Cities', to: '/newcities', private: true },
+  { _id: 3, name: 'New Users', to: '/newusers', private: true },
+  { _id: 5, name: 'Edit Cities', to: '/editcities', private: true },
   { _id: 6, name: 'MyTineraries', to: '/mytineraries' },
   // {_id:4, name: 'UnderConstruction', to: '/underconstruction' },
 ]
@@ -17,6 +21,8 @@ const link = (page) => <LinkRouter className='Header-link' key={page._id} to={pa
 export default function Header() {
   const title = "Welcome to MyTinerary"
   const slogan = "Find your perfect trip, designed by insiders who know and love their cities!"
+  const { user: currentUser } = useAuth();
+  const admin = currentUser?.role === 'admin';
 
   const [open, setOpen] = useState(false)
   const openH = () => {
@@ -26,6 +32,17 @@ export default function Header() {
       setOpen(true)
     }
   }
+
+  const links = [];
+  pages.forEach(page => {
+    if (page.private) {
+      if (admin) {
+        links.push(page);
+      }
+    } else {
+      links.push(page);
+    }
+  });
 
   return (
     <div className='Main-Welcome'>
@@ -37,7 +54,7 @@ export default function Header() {
         <p className='P-Welcome'>{slogan}</p>
       </div>
       <div className='navbar'>
-        {pages.map(link)}
+        {links.map(link)}
       </div>
       <div className="container-hamburger">
         <img onClick={openH} src="/icon-hamburger.png" alt="" width='25px'></img>
@@ -45,7 +62,7 @@ export default function Header() {
           {
             open
               ? <div className='pages-hamburguer'>
-                {pages.map(link)}
+                {links.map(link)}
               </div>
               : null
           }
