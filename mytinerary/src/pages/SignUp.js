1482d9ecce6febdc49c0f7730119226375f14bca
Hector Garay <hectorgaraybascur@gmail.com>
2022-09-15 22:08:21 -0300

    adding props to signUp

--- mytinerary/src/pages/SignUp.js
+++ mytinerary/src/pages/SignUp.js
@@ -2,11 +2,11 @@ import React from 'react'
 import "../styles/SignUp.css"
 import SignUpForm from '../components/SignUpForm'
 
-export default function SignUp() {
+export default function SignUp(props) {
   return (
 
     <div className='form-newcity'>
-      <SignUpForm />
+      <SignUpForm showRole={props.showAdminForm} />
     </div>
   )
 }
