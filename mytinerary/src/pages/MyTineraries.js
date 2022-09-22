f240d98be6239dcadcc05aa0a667add278e8dad3
Hector Garay <hectorgaraybascur@gmail.com>
2022-09-22 17:02:12 -0300

    adding refetch for update likes on mytinerary

--- mytinerary/src/pages/MyTineraries.js
+++ mytinerary/src/pages/MyTineraries.js
@@ -2,18 +2,28 @@ import Itinerary from "../components/Itinerary";
 import BtnModifyItinerary from "../components/BtnModifyItinerary";
 import { useGetItinerariesUserQuery } from '../features/itinerariesAPI';
 import { useParams } from 'react-router-dom';
+import { useEffect, useState } from "react";
 
 export default function Mytineraries() {
-
+  const [reload, setReload] = useState(false)
   const { id } = useParams()
   // const id = "632376967a78bf08031af2df"
-  const { data: itineraries } = useGetItinerariesUserQuery(id)
+  const { data: itineraries, refetch } = useGetItinerariesUserQuery(id)
+  function handleRefetch() {
+    setReload(!reload)
+  }
+  console.log(reload);
+  useEffect(() => {
+    refetch()
+  }, [reload])
+
+
 
   return (
     <div>
       <BtnModifyItinerary />
       {itineraries?.response.map(itinerary =>
-        <Itinerary itinerary={itinerary} key={itinerary._id} />
+        <Itinerary itinerary={itinerary} key={itinerary._id} handleRefetch={handleRefetch} />
       )
       }
     </div>
