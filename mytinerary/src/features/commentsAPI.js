fad520bab1a946dc74533f53f43e53bf73feb76d
juanmoyano123 <76905472+juanmoyano123@users.noreply.github.com>
2022-09-22 10:24:26 -0300

    conectamos itinerary con comments

--- mytinerary/src/features/commentsAPI.js
+++ mytinerary/src/features/commentsAPI.js
@@ -1,33 +1,36 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import url from '../api'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import url from "../api";
 
 export const commentsAPI = createApi({
-    reducerPath: "commentsAPI",
+  reducerPath: "commentsAPI",
 
-    baseQuery: fetchBaseQuery({
-        baseUrl: url
+  baseQuery: fetchBaseQuery({
+    baseUrl: url,
+  }),
+
+  endpoints: (builder) => ({
+    getAllComments: builder.query({
+      query: () => "/comments",
     }),
+    getComments: builder.query({
+      query: (id) => "/comments/" + id,
+    }),
+    getNewComment: builder.mutation({
+      query(comment) {
+        return {
+          url: "/comments/",
+          method: "POST",
+          body: comment,
+          headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+        };
+      },
+    }),
+  }),
+});
 
-    endpoints: (builder) => ({
-        getAllComments: builder.query({
-            query: () => '/comments'
-        }),
-        getComments: builder.query({
-            query: (id) => '/comments/' + id
-        }),
-        getNewComment: builder.mutation({
-            query(comment) {
-              return {
-                url: "/comments",
-                method: "POST",
-                body: comment,
-                headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }
-              };
-            },
-          }),
-    })
-
-})
-
-export default commentsAPI
-export const { useGetAllCommentsQuery, useGetCommentsQuery, useGetNewCommentMutation } = commentsAPI
+export default commentsAPI;
+export const {
+  useGetAllCommentsQuery,
+  useGetCommentsQuery,
+  useGetNewCommentMutation,
+} = commentsAPI;
