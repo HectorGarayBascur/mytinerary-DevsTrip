07d99856615ba9d693cba8f1bf98854805563831
Hector Garay <hectorgaraybascur@gmail.com>
2022-09-22 21:18:43 -0300

    Adding delete comment

--- mytinerary/src/components/Comments.js
+++ mytinerary/src/components/Comments.js
@@ -1,6 +1,52 @@
+import { useAuth } from "../hooks/useAuth";
+import { useDeleteCommentMutation } from '../features/commentsAPI';
+import { toast } from "react-toastify";
+
 import "../styles/Comments.css"
 
-export default function Comments({ comment }) {
+export default function Comments({ comment, refetchComments }) {
+    const { user: currentUser } = useAuth();
+    console.log(comment)
+    const [deleteComment] = useDeleteCommentMutation();
+
+    const handleDelete = async () => {
+        try {
+            const response = await deleteComment(comment._id).unwrap();
+            console.log('=>', response);
+            if (response?.success) {
+                refetchComments();
+                toast.success(response.message, {
+                    position: "top-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: false,
+                    draggable: true,
+                    progress: undefined,
+                });
+            } else {
+                toast.error(response.data?.message, {
+                    position: "top-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: false,
+                    draggable: true,
+                    progress: undefined,
+                })
+            }
+        } catch (error) {
+            toast.error(error.data?.message, {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: true,
+                progress: undefined,
+            })
+        }
+    }
 
     return (
         <div className="container-main-comment">
@@ -15,6 +61,12 @@ export default function Comments({ comment }) {
                     <p>{comment.comment}</p>
                 </div>
             </div>
+            {currentUser?.id === comment.user._id && (
+                <div>
+                    <button onClick={handleDelete}>Delete</button>
+                    <button>Modify</button>
+                </div>
+            )}
         </div>
 
     )
